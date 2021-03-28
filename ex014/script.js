7265914652cd9d96913d201ce6b0e5d77a553730
santo556 <ve727402gafanhoto@gmail.com>
2021-03-28 20:06:03 -0300

    ex014

---
+++ ex014/script.js
@@ -0,0 +1,20 @@
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = 12 //data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >= 0 && hora < 12) {
+        // BOM DIA!
+        img.src = 'Imagens/foto-manha.png'
+        document.body.style.background = '#f2f1ed'
+    } else if (hora >= 12 && hora <= 18) {
+        // BOA TARDE!
+        img.src = 'Imagens/foto-tarde.png'
+        document.body.style.background = '#9a655f'
+    } else {
+        // BOA NOITE!
+        img.src = 'Imagens/foto-noite.png'
+        document.body.style.background = '#120f15'
+    }
+}
