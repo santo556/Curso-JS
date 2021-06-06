87682744ba2ca85772a165ff557bc42cea451463
santo556 <ve727402gafanhoto@gmail.com>
2021-06-05 20:08:53 -0400

    Aula016

---
+++ Aula016/funcao01.js
@@ -0,0 +1,11 @@
+function parimp(n) {
+    if (n % 2 == 0) {
+        return 'par'
+    } else {
+        return 'impar'
+    }
+}
+
+console.log(parimp(553))
+/*let res = parimp(132)
+console.log(res)*/ 
